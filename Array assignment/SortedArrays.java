37a528696325155743c77de0c50eed343c846c54
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-11-19 08:42:30 +0100

    renaming assignment folder

--- Array assignment/SortedArrays.java
+++ Array assignment/SortedArrays.java
@@ -1,40 +1,25 @@
 public class SortedArrays{
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         int[] firstArray = {1, 3, 5};
         int[] secondArray = {2, 4, 6};
         int[] combinedArray = new int[firstArray.length + secondArray.length];
-        int counter1 = 0; 
-        int counter2 = 0; 
-        int counter3 = 0; 
+	for(int count = 0; count < combinedArray.lenght; count++){
+		if(count < firstArray.length){
+			combinedArray[count] = firstArray[count];
+		}
+		else {
+			counter=0;
+			combinedArray[count] = SecondArray[counter];
+			counter++;
+		}
+	
+	for(int index = 0; index < combinedArray.length ; index++ ){
+		System.out.print(combinedArray);
+	}
 
-        while (counter1 < firstArray.length && counter2< secondArray.length) {
-            if (firstArray[counter1] <= secondArray[counter2]) {
-                combinedArray[counter3] = firstArray[counter1];
-                counter1++;
-            } else {
-                combinedArray[counter3] = secondArray[counter2];
-                counter2++;
-            }
-            counter3++;
-        }
 
-         while (counter1< firstArray.length) {
-            combinedArray[counter3] = firstArray[counter3];
-            counter1++;
-            counter3++;
-        }
-
-  
-        while (counter2 < secondArray.length) {
-            combinedArray[counter3] = secondArray[counter2];
-            counter2++;
-            counter3++;
-        }
-
-        System.out.print("Combined sorted array: ");
-        for (int counter = 0; counter < combinedArray.length; counter++) {
-    System.out.print(combinedArray[counter] + " ");
-}
-
-    }
+	}
+	
+	
+	}	
 }
