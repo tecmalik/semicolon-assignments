2f1571b580ea186de3e1a9a470f4d7b2a7174252
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-03 10:28:14 +0100

    Updating phaseGate files

--- PhaseGate/StudentGrade/StudentGrade.java
+++ PhaseGate/StudentGrade/StudentGrade.java
@@ -2,9 +2,22 @@ public class StudentGrade{
 	
 	public static void main(String... args){
 
+		Scanner input = new Scanner(System.in);
+	 
+		System.out.print("Enter number of Students");
+		int studentNumber = input.nextInt();
+		System.out.print("Enter number of Subjects");
+		int numberOfSubjects = input.nextInt();
+		
+		String[] studentNames = new String[studentNumber];
+		int[][] studentGrade = new int [studentGrade][studentNumber][numberOfSubjects]; 
+		
+		for (int count = 0; count<studentNames.length ; count++){
+		
+		System.out.print(" Enter names of Students");
 	
 	
-	
+		}	
 	
 	}
 
--- PhaseGate/StudentGrade/StudentGrand pseudocode.java
+++ PhaseGate/StudentGrade/StudentGrand pseudocode.java
@@ -1,1 +1,16 @@
-StudentGrand pseudocode 
+StudentGrand pseudocode
+ 
+prompt the teacher for the number of student
+collect the number 
+store as number 
+prompt the user for number of subject 
+collect,store as numberOfSubject 
+create an array of array for size of student and subject
+collect the scores for every student and every Subjects
+store and display the class summary 
+
+
+
+N.B
+Constraint: Score must be between 0 and 100.
+Display class summary after input collection.
