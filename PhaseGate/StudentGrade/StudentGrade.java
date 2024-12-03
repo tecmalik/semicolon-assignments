0611b050d923a9588d08a2fef1f4c6cec5ca76ea
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-03 10:50:18 +0100

    Updating phaseGate files

--- PhaseGate/StudentGrade/StudentGrade.java
+++ PhaseGate/StudentGrade/StudentGrade.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class StudentGrade{
 	
 	public static void main(String... args){
@@ -10,15 +11,29 @@ public class StudentGrade{
 		int numberOfSubjects = input.nextInt();
 		
 		String[] studentNames = new String[studentNumber];
-		int[][] studentGrade = new int [studentGrade][studentNumber][numberOfSubjects]; 
+		int[][] studentGrade = new int[studentNumber][numberOfSubjects]; 
 		
-		for (int count = 0; count<studentNames.length ; count++){
+		for(int count = 0; count<studentNames.length ; count++){
 		
-		System.out.print(" Enter names of Students");
-	
-	
+			System.out.print("Enter names of Students : ");
+			studentNames[count] = input.nextLine();
 		}	
 	
+	
+	
+		for(int count = 0; count<.length ; count++){
+	
+			System.out.print("Enter  )
+			for(counter = 0; counter<.length ; count++){
+				
+				
+	
+			}
+			
+		}
+	
+
+
 	}
 
 
