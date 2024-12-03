fbe0d0955e38a7a1c9ab74984f09cd3057db9aa4
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-03 16:10:45 +0100

    Updating phaseGate files

--- PhaseGate/StudentGrade/studentgrade.py
+++ PhaseGate/StudentGrade/studentgrade.py
@@ -1,15 +1,16 @@
-studentNumber = int(input("Enter number of Students"))
-numberOfSubjects = int(input("Enter number of Subjects"))
+studentNumber = int(input("Enter number of Students :"))
+numberOfSubjects = int(input("Enter number of Subjects :"))
 studentNames = []
 studentSubjects = []
 studentGrade = []
-for count in range (1,studentNumber):
-	studentNames.append(imput("Enter name of Student"))
-for count in range (1, numberOfSubjects):
-	studentSubjects.append(imput("Enter name of Student"))
-for count in range (1,studentNumber):
+studentTotal = []
+for count in range (studentNumber):
+	studentNames.append(input("Enter name of Student : "))
+for count in range (numberOfSubjects):
+	studentSubjects.append(input("Enter student Subjects : "))
+for count in range (studentNumber):
 	print("Enter grade "+ studentNames[count])
-	for index in range(1,numberOfSubjects):
-		studentGrade.append(int(imput("Enter name of Student")))
-
+	for index in range(numberOfSubjects):
+		studentGrade.append(int(input("Enter Grade of Student : ")))
+	print("Saving>>>>>>>>>>>>")
 
