4bf27e6f05e942c9459db7c583d1aca2f7e8deb2
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-03 11:18:20 +0100

    Updating phaseGate files

--- PhaseGate/StudentGrade/StudentGrade.java
+++ PhaseGate/StudentGrade/StudentGrade.java
@@ -11,6 +11,7 @@ public class StudentGrade{
 		int numberOfSubjects = input.nextInt();
 		
 		String[] studentNames = new String[studentNumber];
+		String[] studentSubjects = new String[numberOfSubjects];
 		int[][] studentGrade = new int[studentNumber][numberOfSubjects]; 
 		
 		for(int count = 0; count<studentNames.length ; count++){
@@ -18,25 +19,40 @@ public class StudentGrade{
 			System.out.print("Enter names of Students : ");
 			studentNames[count] = input.nextLine();
 		}	
+		
+		for(int count = 0; count<studentSubjects.length ; count++){
+		
+			System.out.print("Enter names of subject Students : ");
+			studentSubjects[count] = input.nextLine();
+		}	
 	
 	
 	
-		for(int count = 0; count<.length ; count++){
-	
-			System.out.print("Enter  )
-			for(counter = 0; counter<.length ; count++){
+		for(int count = 0; count<studentNames.length ; count++){
 				
+			System.out.print("Enter grade studentNames[count]");
+			for(counter = 0; counter<studentSubjects.length ; count++){
 				
+				System.out.print("Enter studentSubjects[counter] grade :");
+					studentGrade[count][counter]  = input.nextInt();
 	
 			}
 			
 		}
+		
+		for()
 	
+
+
+
+
 
 
 	}
 
-
+	
+	
+	
 
 
 
