7eb63e5c306761006b904dcd01bf07b67d765790
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-04 18:45:30 +0100

    updating phase gate file

--- PhaseGate/phone book/PhoneBook.java
+++ PhaseGate/phone book/PhoneBook.java
@@ -6,7 +6,7 @@ public class PhoneBook{
 	
 	 while()
 	
-	
+	 menu(choice)
 
 		
 	
@@ -14,9 +14,30 @@ public class PhoneBook{
 	
 
 
-	public static void menu(){
-	System.out.print(1. Add Contact \n2. Remove Contact \n3. Find contact by phonenumber \n4. Find contact by last name \n5. Edit contact by \nlast name \n6. Edit contact);
-		
+	public static int menu(choice){
+	System.out.print("Sellect AN OPTION: \n1. Add Contact \n2. Remove Contact \n3. Find contact by phonenumber \n4. Find contact by last name \n5. Edit contact by \nlast name \n6. Edit contact \n");
+	int choice = input.nextInt(); 
+		if(choice == 1){
+			
+		}
+		else if(choice == 2){
+			
+		}
+		else if(choice == 3){
+			
+		}
+		else if(choice == 4){
+			
+		}
+
+		else if(choice == 5){
+			
+		}
+		else if(choice == 6){
+			
+		}
+
+
 
 
 	}
