b9a05a8be84e4fb98ba3df8faba37a5f5b257c21
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-04 19:12:52 +0100

    updating phase gate file

--- PhaseGate/phone book/PhoneBook.java
+++ PhaseGate/phone book/PhoneBook.java
@@ -1,12 +1,23 @@
+import java.util.Arraylist;
 import java.util.Scanner;
 public class PhoneBook{
 	public static void main(String[] args){
 	
 	Scanner input = new Scanner(System.in);
+	ArrayList<Integer> phoneNumber = new ArrayList<Integer>();
+	ArrayList<String> adress = new ArrayList<String>();
+	ArrayList<String> contactName = new ArrayList<String>();
+	ArrayList<String> emailAdress = new ArrayList<String>();
+
 	
-	 while()
 	
-	 menu(choice)
+	
+	 while(){
+	
+	menu(choice);
+		
+	
+	}
 
 		
 	
@@ -15,16 +26,16 @@ public class PhoneBook{
 
 
 	public static int menu(choice){
-	System.out.print("Sellect AN OPTION: \n1. Add Contact \n2. Remove Contact \n3. Find contact by phonenumber \n4. Find contact by last name \n5. Edit contact by \nlast name \n6. Edit contact \n");
+	System.out.print("SELLECT AN OPTION: \n1. Add Contact \n2. Remove Contact \n3. Find contact by phonenumber \n4. Find contact by last name \n5. Edit contact by \nlast name \n6. Edit contact \n");
 	int choice = input.nextInt(); 
 		if(choice == 1){
 			
 		}
 		else if(choice == 2){
-			
+			 addToCart();
 		}
 		else if(choice == 3){
-			
+			removeFromCart(contactToRemove);
 		}
 		else if(choice == 4){
 			
@@ -41,6 +52,18 @@ public class PhoneBook{
 
 
 	}
+	
+	public static void addToCart(contactToAdd){
+	
+	
+	
+	}
+	public static void removeFromCart(contactToRemove){
+	
+	
+	
+	}
+
 
 	
 }
