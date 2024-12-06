cabef62e5e04f88f945a6c5d5f378d8bec87491f
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-06 10:42:36 +0100

    updating phase gate files

---
+++ PhaseGate/oldSnacks/CardValidator/CardValidator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+	public class CardValidator{
+
+	static Scanner input = new Scanner(System.in);
+
+	public static void main(String... args){
+	
+		System.out.print("Enter Credit-Card number : ");
+	
+	}
+}
