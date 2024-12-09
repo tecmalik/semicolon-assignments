6729bcf7e6f65a8166fc07c9c9e999d64e6009eb
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-09 11:50:42 +0100

    updating files

--- PhaseGate/StudentGrade/StudentGrade.java
+++ PhaseGate/StudentGrade/StudentGrade.java
@@ -174,20 +174,20 @@ public class StudentGrade{
 
 
 		System.out.println("STUDENT SUMMARY");
-		for(int index = 0; index < numberOfSubjects ; index++){
+		for(int index = 0; index < studentNumber  ; index++){
 			
 	
 				System.out.println( studentSubjects[index] );
 				System.out.print("Highest Scoring Student is");
-				for(int count=0; count <  studentNumber ; count++){
-					if(highestAndLowestPreSubject[count][0] == studentGrade[index][count]){
+				for(int count=0; count < numberOfSubjects ; count++){
+					if(highestAndLowestPreSubject[count][0] == studentGrade[count][index]){
 						System.out.printf("%s Scoring :%d%n",studentNames[count],highestAndLowestPreSubject[count][0]);
 					}
 				}
 				System.out.println("lowest Scoring Student is");
-				for(int count=0; count < studentNumber ; count++){
+				for(int count=0; count < numberOfSubjects ; count++){
 
-					if(highestAndLowestPreSubject[count][1] == studentGrade[index][count]){
+					if(highestAndLowestPreSubject[count][1] == studentGrade[count][index]){
 						System.out.printf("%s Scoring :%d%n",studentNames[count] ,highestAndLowestPreSubject[count][1]);
 					}
 				}
