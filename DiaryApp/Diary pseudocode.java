f2ba70f0c69ed6c682ab714f23411f7acd04a260
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-14 22:11:03 +0100

    updating file

---
+++ DiaryApp/Diary pseudocode.java
@@ -0,0 +1,20 @@
+pseudocode step 1
+
+diary app 
+create a menu page that contains create diary 
+user 
+unlock diary 
+lock diary 
+unlock diary 
+find entry by id 
+add entry 
+delete entry 
+update entry 
+
+step 2:
+create a function to create a diary function :
+prompt the user to Enter a title 
+collect, store as title 
+prompt the user to Enter a date 
+collect the date, store a as date 
+prompt the user to input the text 
---
+++ DiaryApp/Diary.java
@@ -0,0 +1,14 @@
+public class Diary{ 
+	public static void main(String[] args){
+	
+	System.out.print("1.Add Entry\n2. Create Diary\n3. Update entry\n4. find by Id\n5. Unlock Diary\6. Lock Diary\n7. Delete Entry ");
+	
+	
+	}
+
+	public static void Create Diary(){
+		
+	}
+
+	
+}
