a9b82901c0962acd45a07c3842fdefe2a3c028ed
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-14 22:18:52 +0100

    updating file

--- DiaryApp/Diary.java
+++ DiaryApp/Diary.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
 public class Diary{ 
 	public static void main(String[] args){
-	
+	Scanner input = new Scanner(System.in);
+
 	System.out.print("1.Add Entry\n2. Create Diary\n3. Update entry\n4. find by Id\n5. Unlock Diary\6. Lock Diary\n7. Delete Entry ");
 	
 	
 	}
 
 	public static void Create Diary(){
+		System.out.print("Enter Title : ");
 		
-	}
 
 	
 }
