5ae950ec408ddd723aba49b1adc82b87b7d09a50
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-15 21:52:33 +0100

    commiting assignmen

--- DiaryApp/Diary.java
+++ DiaryApp/Diary.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 public class Diary{ 
+
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args){
-	Scanner input = new Scanner(System.in);
 		
-	System.out.print("welcomr to My Diary App");
+	System.out.print("welcome to My Diary App");
 	System.out.print("Enter username : ");
 	String userName = input.next();
 	System.out.print("password : ");
-	String userName = input.next();
-	System.out.print("1.Add Entry\n2. Create Diary\n3. Update entry\n4. find by Id\n5. Unlock Diary\6. Lock Diary\n7. Delete Entry ");
+	String userpassword = input.next();
+	}
+	public static void menu(){ 
+	System.out.print("1.Add Entry\n2. Create Diary\n3. Update entry\n4. find by Id\n5. Unlock Diary\6. Lock Diary\n7. Delete \n Entry Enter a number :");
+	int userSelection = input.nextint();
+	if (userSelection == 1){}
 	
 	
 	}
 	
-	public static setpassword(){
+	public static void setpassword(){
 		System.out.print("Enter a new password : ");
 		String firstInput = input.next(); 
 		System.out.print("Enter password again: ");
-		String. secondInput = input.next(); 
+		String secondInput = input.next(); 
 		if (firstInput.equals(secondInput)){
 			String actualPassword = firstInput;
 		}else{
@@ -26,20 +32,20 @@ public class Diary{
 		}
 	}
 
-	public static void Create Diary(){
+	public static void createDiary(){
 		System.out.print("Enter Title : ");
 		String title = input.nextLine();
-		System.out.print("Enter Date in \"yy/mm/dd\" :  ");
-		String date = iniput.nextline()
+		System.out.print("Enter Date in \"yy/mm/dd\" : ");
+		String date = input.nextLine();
 		System.out.print("Enter text");
 	}	
 	
-	public static void unlockDiary(){
+	public static void unlockDiary(String actualPassword){
 		
 		System.out.print("Enter password : ");
-		String userPassword = inpuut.next();
-		if (userpassword.equals(actualPassword)){
-			Create Diary();
+		String userPassword = input.next();
+		if (userPassword.equals(actualPassword)){
+			createDiary();
 		}else{
 			System.out.print("invalid passWord");
 		}
@@ -50,7 +56,7 @@ public class Diary{
 		System.out.print(" Diary locked ");	
 	}
 	
-	public static findByID(){
+	public static void findByID(){
 			
 	} 
 
