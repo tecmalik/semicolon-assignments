4ae1d2da2ffb4ddd0850eea60130b73cde861e89
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-28 17:53:20 +0100

    updating files

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 public class Tiktactoe{
-	Static Scanner input = new Scanner(System.in);
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args){
-		
+		String[][] board = ["-","-","-",
+				    "-","-","-",
+				    "-","-","-"];
+	
 	}
+	public static void main 
 }
