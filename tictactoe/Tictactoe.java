e07f41a15f1da00af80c3dc3a087008c7c06b5df
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-30 10:38:44 +0100

    updating files

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 public class Tiktactoe{
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args){
-		String[][] board = ["-","-","-",
-				    "-","-","-",
-				    "-","-","-"];
+		String[][] board = {
+				    {"-","-","-"},
+				    {"-","-","-"},
+				    {"-","-","-"}
+					};
+				     
+
 		String firstPlayer = "x";
 		string winner = " ";
+		Boolean gameisRunning = true;
 		
 		
-		while(gameisrunning){
+		while(gameisRunning){
 
 			printGameBoard(board);
 			playerInput(board);
@@ -18,7 +23,7 @@ public class Tiktactoe{
 
 
 	}
-	public static void printGameBoard(board){
+	public static void printGameBoard(String[][] board){
 		System.out.printf("%s | %s | %s%n ",board[0],board[1],board[2]);
 		System.out.print("---------------------");
 		System.out.printf("%s | %s | %s%n ",board[3],board[4],board[5]);
@@ -26,7 +31,7 @@ public class Tiktactoe{
 		System.out.printf("%s | %s | %s%n ",board[6],board[7],board[8]);
 	
 	}
-	public static void playerInput(board):
+	public static void playerInput([][]String board){
 		System.out.print("Enter a number 1 - 9 : ");
 		int playerInput = input.nextInt();
 		if(playerInput >= 1 && playerInput <= 9 && board[playerInput-1] == "-" ){
