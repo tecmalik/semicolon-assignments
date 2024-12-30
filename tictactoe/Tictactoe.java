98151afe1df04c6021bf85555f369fed36eb200e
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-30 10:01:18 +0100

    updating files

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -5,7 +5,30 @@ public class Tiktactoe{
 		String[][] board = ["-","-","-",
 				    "-","-","-",
 				    "-","-","-"];
+		String firstPlayer = "x"
+		string winner = None
 	
 	}
-	public static void main 
+	public static void printGameBoard(board){
+		System.out.printf("%s | %s | %s%n ",board[0],board[1],board[2]);
+		System.out.printf("%s | %s | %s%n ",board[3],board[4],board[5]);
+		System.out.printf("%s | %s | %s%n ",board[6],board[7],board[8]);
+	
+	}
+	public ststic void 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
