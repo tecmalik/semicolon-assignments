a5305dd3d3ad1bfd0b930388fc3a00650079ffb8
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-31 20:53:55 +0100

    updating files

--- TicTacToe2D/TicTacToe2D.java
+++ TicTacToe2D/TicTacToe2D.java
@@ -12,7 +12,6 @@ public class TicTacToe2D{
 
 	public static void main(String[] args){
 
-
 		
 		
 		while(gameisRunning){
@@ -27,7 +26,7 @@ public class TicTacToe2D{
 
 	}
 	public static void printGameBoard(String[][] BOARD){
-		for(int count = 0 ; count< BOARD.length ){
+		for(int count = 0 ; count< BOARD.length ; count++ ){
 		System.out.printf("%s | %s | %s \n ",BOARD[count][1],BOARD[count][2],BOARD[count][3]);
 			if(count<=2){
 				System.out.println("---------");
@@ -43,10 +42,10 @@ public class TicTacToe2D{
 		if(playerInput >= 11 && playerInput <= 13 && BOARD[playerInput-1] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
-		if(playerInput >= 21 && playerInput <= 23 && BOARD[playerInput-1] == "EMPTY" ){
+		else if(playerInput >= 21 && playerInput <= 23 && BOARD[playerInput-1] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
-		if(playerInput >= 31 && playerInput <= 33 && BOARD[playerInput-1] == "EMPTY" ){
+		else if(playerInput >= 31 && playerInput <= 33 && BOARD[playerInput-1] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
 
@@ -57,16 +56,16 @@ public class TicTacToe2D{
 	}
 	
 	public static boolean checkHorizontal(String[][] BOARD){
-		if(BOARD[0] == BOARD[1] && BOARD[0]  == BOARD[2] && BOARD[0] != "EMPTY"){
-			winner = board[2];
+		if(BOARD[0][0] == BOARD[0][1] && BOARD[0][0]  == BOARD[0][2] && BOARD[0][0] != "EMPTY"){
+			winner = board[0][0];
 			return true ;
 		}
-		else if(BOARD[3] == BOARD[4] && BOARD[3]  == BOARD[5] && BOARD[3] != "EMPTY"){
-			winner = board[2];
+		else if(BOARD[1][0] == BOARD[1][1] && BOARD[1][0]  == BOARD[1][2] && BOARD[1][0] != "EMPTY"){
+			winner = board[1][0];
 			return true ;
 		}
-		else if(BOARD[6] == BOARD[7] && BOARD[6]  == BOARD[8] && BOARD[6] != "EMPTY"){
-			winner = BOARD[2];
+		else if(BOARD[2][0] == BOARD[2][1] && BOARD[2][0]  == BOARD[2][2] && BOARD[2][0] != "EMPTY"){
+			winner = BOARD[2][0];
 			return true ;
 		}
 
@@ -75,16 +74,16 @@ public class TicTacToe2D{
 
 
 	public static boolean checkvertical(String[][] board){
-		if(BOARD[0] == BOARD[3] && board[0]  == board[6] && board[0] != "EMPTY"){
-			winner = board[0];
+		if(BOARD[0][0] == BOARD[1][0] && BOARD[0][0]  == BOARD[2][0] && BOARD[0][0] != "EMPTY"){
+			winner = BOARD[0];
 			return true ;
 		}
-		else if(board[1] == board[4] && board[1]  == board[7] && board[1] != "EMPTY"){
+		else if(BOARD[1] == BOARD[4] && BOARD[1]  == BOARD[7] && BOARD[1] != "EMPTY"){
 			winner = board[1];
 			return true ;
 		}
-		else if(board[2] == board[5] && board[2]  == board[8] && board[2] != "EMPTY"){
-			winner = board[2];
+		else if(BOARD[2] == BOARD[5] && BOARD[2]  == BOARD[8] && BOARD[2] != "EMPTY"){
+			winner = BOARD[2];
 			return true ;
 		}
 		return false;
@@ -92,21 +91,21 @@ public class TicTacToe2D{
 	
 	public static boolean checkDiagonals(String[][] board){
 	
-		if(board[0] == board[4] && board[0]  == board[8] && board[0] != "EMPTY"){
-			winner = board[0];
+		if(BOARD[0] == BOARD[4] && BOARD[0]  == BOARD[8] && BOARD[0] != "EMPTY"){
+			winner = BOARD[0];
 			return true ;
 		}
-		else if(board[2] == board[4] && board[1]  == board[7] && board[1] != "EMPTY"){
-			winner = board[2];
+		else if(BOARD[2] == BOARD[4] && BOARD[1]  == BOARD[7] && BOARD[1] != "EMPTY"){
+			winner = BOARD[2];
 			return true ;
 		}
 		
 		return false;
 	}
 
-	public static void checkTie(String[][] board){
-		for(int count = 0; count < board.length ; count++){
-			if (board[count].equals("EMPTY") ){
+	public static void checkTie(String[][] BOARD){
+		for(int count = 0; count < BOARD.length ; count++){
+			if (BOARD[count].equals("EMPTY") ){
 				
 				return;
 			}
@@ -130,7 +129,7 @@ public class TicTacToe2D{
 	}
 
 	public static void checkWin(){
-		if (checkvertical(board) || checkDiagonals(board) || checkHorizontal(board)){
+		if (checkvertical(BOARD) || checkDiagonals(BOARD) || checkHorizontal(BOARD)){
 			System.out.printf("the winner is %s%n",winner);		
 	
 		}
