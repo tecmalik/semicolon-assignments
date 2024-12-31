77c8afe6028d9e31690ee2f8c8961296ada989c3
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-31 17:46:11 +0100

    updating files

--- TurtleGraphics/TurtleGraphics.java
+++ TurtleGraphics/TurtleGraphics.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class TurtleGraphics{
 		
 	static Scanner input = new Scanner(System.in);
+	static int runningProgram = 0;
 
 	public static void main(String[] args){
 		int[][] floor = new int[20][20];
-		int userChoice = 0;
-		while (userChoice != 9){
+		
+		while (runningProgram != 9){
 			
 			 commandMenu();
 			
@@ -16,15 +17,42 @@ public class TurtleGraphics{
 	}
 
 
-	public static void commandMenu(){
+	public static int commandMenu(){
 		System.out.print("1. Pen up.\n2. Pen down.\n3. Turn right.\n4. Turn left \n5,10. Moving foward 10 spaces(replace 20 doe diffrent number of spaces).\n6. DIsplay the 20-by-20 array.\n9End of data(sentinel) ");
 		int userChoice = input.nextInt();
-		if (userChoice == 1){
+
+
+	}
+
+	public ststic void action(commandMenu())
+		if (commandMenu() == 1){
 			
 		}
-		if (userChoice == 2){
+		else if (commandMenu() == 2){
 			
 		}
+		else if (commandMenu() == 3){
+			
+		}
+
+		else if (commandMenu() == 4){
+			
+		}
+
+		else if (commandMenu() == 5 ){
+		
+		}
+		else if (commandMenu() == 10){
+			
+		}
+
+		else if (commandMenu() == 6 ){
+			
+		}
+		else if (commandMenu() == 9){
+			runningProgram = 9;
+		}
+
 
 	}
 
