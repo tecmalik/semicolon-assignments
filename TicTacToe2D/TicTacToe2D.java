3e9ca40ba67672169411b2d91f8c060e0977ecaf
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-31 21:40:37 +0100

    updating files

--- TicTacToe2D/TicTacToe2D.java
+++ TicTacToe2D/TicTacToe2D.java
@@ -29,7 +29,7 @@ public class TicTacToe2D{
 		for(int count = 0 ; count< BOARD.length ; count++ ){
 		System.out.printf("%s | %s | %s \n ",BOARD[count][0],BOARD[count][1],BOARD[count][2]);
 			if(count<=2){
-				System.out.println("---------");
+				System.out.println("--------------------");
 			}
 		}
 			
@@ -39,13 +39,13 @@ public class TicTacToe2D{
 		System.out.print("Enter row number and column numberyy \ne.g 12 , 13\n  : ");
 		int playerInput = input.nextInt();
 
-		if(playerInput >= 11 && playerInput <= 13 && BOARD[playerInput-1] == "EMPTY" ){
+		if(playerInput >= 11 && playerInput <= 13 && BOARD[playerInput-1/10][playerInput-1%10] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
-		else if(playerInput >= 21 && playerInput <= 23 && BOARD[playerInput-1] == "EMPTY" ){
+		else if(playerInput >= 21 && playerInput <= 23 && BOARD[playerInput-1/10][playerInput-1%10] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
-		else if(playerInput >= 31 && playerInput <= 33 && BOARD[playerInput-1] == "EMPTY" ){
+		else if(playerInput >= 31 && playerInput <= 33 && BOARD[playerInput-1/10][playerInput-1%10] == "EMPTY" ){
 			BOARD[playerInput-1/10][playerInput-1%10] = firstPlayer;
 		}
 
@@ -57,11 +57,11 @@ public class TicTacToe2D{
 	
 	public static boolean checkHorizontal(String[][] BOARD){
 		if(BOARD[0][0] == BOARD[0][1] && BOARD[0][0]  == BOARD[0][2] && BOARD[0][0] != "EMPTY"){
-			winner = board[0][0];
+			winner = BOARD[0][0];
 			return true ;
 		}
 		else if(BOARD[1][0] == BOARD[1][1] && BOARD[1][0]  == BOARD[1][2] && BOARD[1][0] != "EMPTY"){
-			winner = board[1][0];
+			winner = BOARD[1][0];
 			return true ;
 		}
 		else if(BOARD[2][0] == BOARD[2][1] && BOARD[2][0]  == BOARD[2][2] && BOARD[2][0] != "EMPTY"){
@@ -104,10 +104,12 @@ public class TicTacToe2D{
 	} 
 
 	public static void checkTie(String[][] BOARD){
-		for(int count = 0; count < BOARD.length ; count++){
-			if (BOARD[count][].equals("EMPTY") ){
+		for(int index = 0; index < BOARD.length ; index++){
+			for(int count = 0; count < BOARD.length ; count++){
+				if (BOARD[index][count].equals("EMPTY") ){
 				
-				return;
+					return;
+				}
 			}
 		}
 		System.out.println("Draw");
