d8c78c133a31ba01b8c2593898a47aa1a0b9021d
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2024-12-31 16:22:20 +0100

    updating files

--- TurtleGraphics/TurtleGraphics.java
+++ TurtleGraphics/TurtleGraphics.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 public class TurtleGraphics{
+		
+	static Scanner input = new Scanner(System.in);
 	public static void main(String){
 		int[][] floor = new int[20][20];
-	
-		while ( != 9){
+		int continue = 0;
+		while ( continue != 9){
 			
 			command();
+			
 		}
 		
 	
@@ -15,7 +19,11 @@ public class TurtleGraphics{
 		System.out.print("1. Pen up.\n2. Pen down.\n3. Turn right.\n4. Turn left \n5,10. Moving foward 10 spaces(replace 20 doe diffrent number of spaces).\n6. DIsplay the 20-by-20 array.\n9End of data(sentinel) ");
 	}
 
-
+	public Static penUp(){
+	
+	
+	}
+	
 
 
 			
