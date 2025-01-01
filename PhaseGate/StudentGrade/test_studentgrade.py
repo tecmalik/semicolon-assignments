235e5f502c17fe27f5bf0006933a4237edd900cf
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-01 22:27:24 +0100

    updating files

--- PhaseGate/StudentGrade/test_studentgrade.py
+++ PhaseGate/StudentGrade/test_studentgrade.py
@@ -5,9 +5,11 @@ class StudentGrade(TestCase):
 	def test_if_getEachStudentTotal_exist(self):
 		studentgrade.getEachStudentTotal(["maths","english"] , {malik:{maths:56},jame:{english:45}})
 	def test_if_getEachStudentTotal_returns_a_list(self):
-		studentgrade.getEachStudentTotal(["maths","english"] , {malik:{maths:56,english:50},jame:{maths:30 , english:45}})
+		value = studentgrade.getEachStudentTotal(["maths","english"] , {malik:{maths:56,english:50},jame:{maths:30 , english:45}})
 		expected = [106, 75]
 		self.assertEqual(value,expected)
 
-
-	
+	def test_if_getPosition_exist(self):
+		value = studentgrade.getPosition([23,4,5,67,9])
+		expected
+		
