30f920e3f01eb7225a113fa9e88d7e54a36b9365
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-01 15:05:45 +0100

    updating phase gate files

--- CompondIntrestCalculator/CompoundIntrestCalculator.java
+++ CompondIntrestCalculator/CompoundIntrestCalculator.java
@@ -7,19 +7,19 @@ public class CompoundIntrestCalculator{
 	Scanner input = new Scanner(System.in);
 	//BigdDecimal bigdecimal = new BigDecimal("12345");
 	
-	System.out.print(" Initial Investment ");
+	System.out.print(" Initial Investment * ");
 	System.out.print("Amount of money that you have available to invest instantlly. ");
-	int initialInvestment = input.nextInt();
+	double initialInvestment = input.nextDouble();
 	
 	System.out.print("Monthly Constribution ");
 	System.out.print("Amount that you plan to add to the principal every month,or a nagative \n number for the amount that you plan to withdraw envry month.");
 	int monthlyContribution = input.nextInt();
 	
-	System.out.print("Length of Tume in Years");
+	System.out.print("Length of Tume in Years *");
 	System.out.print("Length of time, in years, that you plan to save");
 	int LengthInYears = input.nextInt();
 	
-	System.out.print("Estimated Intrest Rate");
+	System.out.print("Estimated Intrest Rate *");
 	System.out.print("Your estimated annual intrest rate");
 	int estimatesIntrestRate = input.nextInt();
 	double intrestRate = (double)estimatesIntrestRate /100 ;
@@ -32,7 +32,7 @@ public class CompoundIntrestCalculator{
 	System.out.print(" Times per year that intrest will be compounded");
 	int compoundFrequency = input.nextInt();
 	
-	compound intrest = 
+	compoundIntrestAmount = initialInvestment(1 + (((double)estimatesIntrestRate /100)/LengthInYears))
 	
 	}
 	
