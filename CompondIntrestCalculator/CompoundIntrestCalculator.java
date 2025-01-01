65d92346166d9c64898c5048160374aa146de796
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-01 15:20:00 +0100

    updating phase gate files

--- CompondIntrestCalculator/CompoundIntrestCalculator.java
+++ CompondIntrestCalculator/CompoundIntrestCalculator.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;
-import java.math.BigDecimal;
-import java.text.NumberFormart;
+
 public class CompoundIntrestCalculator{
 	public static void main(String[] args){
 	
 	Scanner input = new Scanner(System.in);
-	//BigdDecimal bigdecimal = new BigDecimal("12345");
 	
-	System.out.print(" Initial Investment * ");
-	System.out.print("Amount of money that you have available to invest instantlly. ");
+	System.out.println(" Initial Investment * ");
+	System.out.println("Amount of money that you have available to invest instantlly. ");
 	double initialInvestment = input.nextDouble();
 	
-	System.out.print("Monthly Constribution ");
-	System.out.print("Amount that you plan to add to the principal every month,or a nagative \n number for the amount that you plan to withdraw envry month.");
+	System.out.println("Monthly Constribution ");
+	System.out.println("Amount that you plan to add to the principal every month,or a nagative \n number for the amount that you plan to withdraw envry month.");
 	int monthlyContribution = input.nextInt();
 	
-	System.out.print("Length of Tume in Years *");
-	System.out.print("Length of time, in years, that you plan to save");
+	System.out.println("Length of Tume in Years *");
+	System.out.println("Length of time, in years, that you plan to save");
 	int LengthInYears = input.nextInt();
 	
-	System.out.print("Estimated Intrest Rate *");
-	System.out.print("Your estimated annual intrest rate");
-	int estimatesIntrestRate = input.nextInt();
-	double intrestRate = (double)estimatesIntrestRate /100 ;
+	System.out.println("Estimated Intrest Rate in % *");
+	System.out.println("Your estimated annual intrest rate");
+	double estimatesIntrestRate = input.nextDouble();
 
-	System.out.print("Intrest rate Varience range");
-	System.out.print("Range of interest rate(above and beliow the rate set above) that you desire to see results for.");
+	System.out.println("Intrest rate Varience range");
+	System.out.println("Range of interest rate(above and beliow the rate set above) that you desire to see results for.");
 	int intrestRateVarianceRange = input.nextInt();
 	
-	System.out.print("compound Frequency");
-	System.out.print(" Times per year that intrest will be compounded");
+	System.out.println("compound Frequency");
+	System.out.println(" Times per year that intrest will be compounded");
 	int compoundFrequency = input.nextInt();
 	
-	compoundIntrestAmount = initialInvestment(1 + (((double)estimatesIntrestRate /100)/LengthInYears))
+	compoundIntrestAmount = initialInvestment * (Math.pow((1 + ((estimatesIntrestRate /100)/LengthInYears)), LengthInYears*compoundFrequency ))
 	
 	}
 	
