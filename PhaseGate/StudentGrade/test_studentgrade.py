d8356b43c3268e1a67439fb55bed2998aafa3cb8
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-02 23:39:29 +0100

    updating files

--- PhaseGate/StudentGrade/test_studentgrade.py
+++ PhaseGate/StudentGrade/test_studentgrade.py
@@ -6,7 +6,7 @@ class StudentGrade(TestCase):
 		value = studentgrade.getEachStudentTotal(["maths","english"], {"malik":{"maths":56},"jame":{"english":45}})
 
 	def test_if_getEachStudentTotal_returns_a_list(self):
-		value = studentgrade.getEachStudentTotal(["maths","english"] , {malik:{maths:56,english:50},jame:{"maths":30 , "english":45}})
+		value = studentgrade.getEachStudentTotal(["maths","english"] , {malik:{maths:56,english:50},james:{"maths":30 , "english":45}})
 		expected = [106, 75]
 		self.assertEqual(value,expected)
 
@@ -20,7 +20,13 @@ class StudentGrade(TestCase):
 	def test_if_getPosition_will_return_a_list(self):
 		value = studentgrade.getPosition([23,4,5,67,9])
 		expected = [6,1,2,5,3]
+		self.assertEqual(value,expected)
 	
 	def test_if_subjectTotal_exist(self):
-		value = studentgrade.subjectTotal()
+		value = studentgrade.subjectTotal({"malik":{"maths":56,"english":45},"jame":{"maths":45,"english":45}})
+
+	def test_if_subjectTotal_exist(self):
+		value = studentgrade.subjectTotal({"malik":{"maths":56,"english":45},"jame":{"maths":45,"english":45}})
+		expected = [101, 90]
+		self.assertEqual(value,expected)
 		
