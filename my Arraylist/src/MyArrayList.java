2b40870ce1769f3b6758cf870313cd84ba03a8af
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-21 22:04:12 +0100

    updating files

--- my Arraylist/src/MyArrayList.java
+++ my Arraylist/src/MyArrayList.java
@@ -1,3 +1,69 @@
 public class MyArrayList {
+    private String[] elements = new String[3];
+    private int size = 0;
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
+    public void add(String item) {
+        if (elements.length-1 == size) {
+        String[] newElements = new String[elements.length*2];
+        for(int index = 0; index < size; index++) {
+            newElements[index] = elements[index];
+        }
+         elements = newElements;
+    }
+        this.elements[size] = item ;
+        this.size++;
+
+
+    }
+
+    public String get(int index) {
+    return this.elements[index];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int getIndexOf(String malik) {
+        for(int count = 0; count < size; count++) {
+            if(this.elements[count].equals(malik)) {
+                return count;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(String element) {
+        for(int count = 0; count < size; count++) {
+            if(this.elements[count].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(String element) {
+        if(this.contains(element)) {
+            for(int count = 0; count < size-1; count++) {
+                if(element.equals(elements[count]) && count != size-1 ) {
+                    elements[count] = elements[count+1];
+                    elements[count+1] = null;
+                }
+                else if(count == size-1) {
+                    elements[count] = null;
+                }
+            }
+            size--;
+        }
+    }
+
+    public void clear() {
+        for(int count = 0; count < size; count++) {
+            elements[count] = null;
+            size = 0;
+        }
+    }
 }
--- my Arraylist/test/TestMyArraylist.java
+++ my Arraylist/test/TestMyArraylist.java
@@ -1,12 +1,70 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.processing.SupportedAnnotationTypes;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMyArraylist {
-    @Test
+    MyArrayList myArrayList ;
+    @BeforeEach
     public void test_MyArrayList() {
-        MyArrayList myarrayList = new MyArrayList();
+        myArrayList = new MyArrayList();
     }
     @Test
-    public void test_() {}
+    public void test_myArrayListIsEmpty() {
+        assertTrue(myArrayList.isEmpty());
+    }
+    @Test
+    public void test_myArrayListCanAcceptAnElement() {
+        myArrayList.add("man");
+        assertFalse(myArrayList.isEmpty());
+    }
+    @Test
+    public void test_myArrayListCanRemoveAnElement() {
+        myArrayList.add("man");
+        myArrayList.add("malik");
+        assertEquals("man",myArrayList.get(0));
+        assertEquals("malik",myArrayList.get(1));
+    }
+    @Test
+    public void test_thatMyArrayListWillReturnAnIndex() {
+        myArrayList.add("man");
+        myArrayList.add("malik");
+        myArrayList.add("ojo");
+        assertEquals(1,myArrayList.getIndexOf("malik"));
+    }
+    @Test
+    public void test_myArrayListCanContainAnElement() {
+        myArrayList.add("school");
+        myArrayList.add("matin");
+        assertTrue(myArrayList.contains("matin"));
+    }
+    @Test
+    public void test_myArrayListCanRemoveElement() {
+        myArrayList.add("man");
+        myArrayList.add("malik");
+        myArrayList.add("ojo");
+        myArrayList.remove("malik");
+        assertFalse(myArrayList.contains("malik"));
+        assertEquals("ojo",myArrayList.get(1));
+
+    }
+    @Test
+    public void test_myArrayListCanBeCleared() {
+        myArrayList.add("jug");
+        myArrayList.add("maliki");
+        myArrayList.add("ant");
+        myArrayList.clear();
+        assertTrue(myArrayList.isEmpty());
+    }
+    @Test
+    public void test_myArrayListCanAddFiveElement() {
+        myArrayList.add("man");
+        myArrayList.add("malik");
+        myArrayList.add("ojo");
+        myArrayList.add("jug");
+        myArrayList.add("ant");
+        assertEquals(5,myArrayList.size());
+    }
+
+
 }
