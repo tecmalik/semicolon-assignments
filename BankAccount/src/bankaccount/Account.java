e604879f3277862852b17576f5a6a8b485e68c4e
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-27 21:44:04 +0100

    updating files

--- BankAccount/src/bankaccount/Account.java
+++ BankAccount/src/bankaccount/Account.java
@@ -1,7 +1,24 @@
 package bankaccount;
 
 public class Account {
+    private String firstName;
+    private String lastName;
     private int balance = 0;
+    private String pinCode;;
+
+    public Account(String firstName, String lastName ,String pinCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pinCode = pinCode;
+    }
+
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastName;
+    }
 
     public boolean isEmpty() {
         return balance == 0;
@@ -15,6 +32,15 @@ public class Account {
         this.balance += amount;
     }
 
+    public int checkBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount, String pinCode) {
+        if(this.pinCode.equals(pinCode)){
+            this.balance -= amount;
+        }
+    }
 
 
 }
--- BankAccount/test/TestAccount.java
+++ BankAccount/test/TestAccount.java
@@ -2,32 +2,44 @@ import bankaccount.Account;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAccount {
     private Account account;
     @BeforeEach
     public void test_Account() {
-        account = new Account();
+        account = new Account("malik","ojo","1234");
     }
     @Test
-    public void test_AccountBalenceIsEmpty() {
+    public void test_AccountHasAName() {
+        assertEquals("malik",account.getFirstname());
+        assertEquals("ojo",account.getLastname());
+    }
+    @Test
+    public void test_AccountBalanceIsEmpty() {
         assertTrue(account.isEmpty());
     }
     @Test
-    public void test_AccountBalenceIsNotEmpty() {
+    public void test_AccountBalanceIsanotEmpty() {
         account.deposit(1000);
         assertTrue(account.isNotEmpty());
     }
     @Test
-    public void test_AccountBalanceIsNotEmpty() {
+    public void test_AccountBalanceIsEmptyWillReturnFalse() {
         account.deposit(1000);
         assertFalse(account.isEmpty());
     }
     @Test
-    public void test_AccountCanWithdraw() {
+    public void test_AccountCanCheckBalance() {
         account.deposit(1000);
-        account.withdraw()
+        assertEquals(1000,account.checkBalance());
     }
+    @Test
+    public void test_AccountCanDepositAndWithdraw() {
+        account.deposit(1000);
+        account.withdraw(1000,"1234");
+        assertEquals(0,account.checkBalance());
+    }
+    
+
 }
