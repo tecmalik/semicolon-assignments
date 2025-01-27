b10f6f638fbd458554d242590631d47ce481f1cd
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-01-27 18:46:56 +0100

    updating files

--- AutomaticBike/src/AutomaticBike.java
+++ AutomaticBike/src/AutomaticBike.java
@@ -7,12 +7,16 @@ public class AutomaticBike {
     private int speed = 0;
 
 
-    public boolean powerOff(){
+    public boolean powerIsOff(){
         return this.power == 0 ;
     }
 
-    public boolean powerOn() {
+    public boolean powerIsOn() {
        return this.power == 1 ;
+    }
+
+    public void powerOn() {
+        this.power = 1;
     }
 
     public int getGear() {
@@ -26,7 +30,36 @@ public class AutomaticBike {
         return this.speed;
     }
 
-    public void IncreaseGear() {
-        this.gear++;
+    public void increaseGear() {
+        if (powerIsOn()) {
+            this.gear++;
+        }
+
     }
+
+    public void accelerate() {
+        if (powerIsOn()) {
+            if( this.gear == 0 ){
+                increaseGear();
+            }
+             this.speed+=gear;
+
+            if (this.speed == 21) {
+                 this.gear++;
+             }
+             else if (this.speed == 31) {
+                 this.gear++;
+             }
+             else if (this.speed == 40) {
+                 this.gear++;
+            }
+
+
+        }
+    }
+
+    public void powerOff() {
+        this.power = 0;
+    }
+
 }
--- AutomaticBike/test/TestAutomaticBike.java
+++ AutomaticBike/test/TestAutomaticBike.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestAutomaticBike {
     AutomaticBike automaticBike;
@@ -12,25 +11,84 @@ public class TestAutomaticBike {
     }
     @Test
     public void test_ThatMyBikeIsOff(){
-        assertTrue(automaticBike.powerOff());
+        assertTrue(automaticBike.powerIsOff());
     }
     @Test
     public void test_ThatMyBikeCanOn(){
-        assertTrue(automaticBike.powerOn());
+        automaticBike.powerOn();
+        assertTrue(automaticBike.powerIsOn());
     }
     @Test
     public void test_ThatMyBikeCan_is_not_in_motion(){
-        automaticBike.getGear();
         assertEquals(0,automaticBike.getGear());
-        automaticBike.getAcceleration();
-        assertEquals(0,automaticBike.getGear());
-        automaticBike.getSpeed();
+        assertEquals(0,automaticBike.getAcceleration());
         assertEquals(0,automaticBike.getSpeed());
     }
     @Test
-    public void test_thatMyBikeCan_drive(){
-        automaticBike.IncreaseGear();
-        automaticBike.getAcceleration();
+    public void test_thatMyBikeIsOffICantIncreaseGear(){
+        automaticBike.powerIsOff();
+        automaticBike.increaseGear();
+        assertTrue(automaticBike.powerIsOff());
+        assertFalse(automaticBike.powerIsOn());
+        assertEquals(0,automaticBike.getGear());
+    }
+    @Test
+    public void test_thatMyBikeIsOnAndCanIncreaseToGearOne (){
+        automaticBike.powerOn();
+        automaticBike.increaseGear();
+        automaticBike.accelerate();
+        assertEquals(1,automaticBike.getGear());
+        assertEquals(1,automaticBike.getSpeed());
+
+    }
+    @Test
+    public void test_thatWhenMyBikeIsOnGear_1_AccelerationContineToIncermentByOne(){
+        automaticBike.powerOn();
+        automaticBike.increaseGear();
+        automaticBike.accelerate();
+        automaticBike.accelerate();
+        automaticBike.accelerate();
+        assertEquals(1,automaticBike.getGear());
+        assertEquals(3,automaticBike.getSpeed());
+    }
+    @Test
+    public void test_thatMyBikeCanBePoweredOnAndPoweredOff(){
+        assertTrue(automaticBike.powerIsOff());
+        automaticBike.powerOn();
+        assertTrue(automaticBike.powerIsOn());
+        automaticBike.powerOff();
+        assertFalse(automaticBike.powerIsOn());
+    }
+    @Test
+    public void test_thatWhenMyBikeSpeedCanIncreaseAndChangeToTheNextGear(){
+        automaticBike.powerOn();
+        automaticBike.increaseGear();
+        automaticBike.increaseGear();
+        assertEquals(2,automaticBike.getGear());
+    }
+    @Test
+    public void test_myBikeCanSwitchToGear2AutomaticallyAsTheTheSpeedIncreasesAbove21(){
+        automaticBike.powerOn();
+        for(int counter = 0; counter < 21; counter++){
+            automaticBike.accelerate();
+        }
+        assertEquals(2,automaticBike.getGear());
+    }
+    @Test
+    public void test_myBikeCanSwitchToGear3AutomaticallyAsTheSpeedIncreasesAbove31(){
+        automaticBike.powerOn();
+        for(int counter = 0; counter < 31; counter++){
+            automaticBike.accelerate();
+        }
+        assertEquals(3,automaticBike.getGear());
+    }
+    @Test
+    public void test_myBikeCanSwitchToGear4AutomaticallyAsTheSpeedIncreasesAbove41(){
+        automaticBike.powerOn();
+        for(int counter = 0; counter < 41; counter++){
+            automaticBike.accelerate();
+        }
+        assertEquals(4,automaticBike.getGear());
     }
 
 
