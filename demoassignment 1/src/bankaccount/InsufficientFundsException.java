578555a5fa122e4c876ea6e160e98ebc1af05429
Abdulmalik Ojo <abdulmalikojo2@gmail.com>
2025-02-09 00:11:10 +0100

    updating my bank files

---
+++ demoassignment 1/src/bankaccount/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package bankaccount;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
